cc7903a2657c63c1866e47c9714a5b264aaefb5c
DaltonAPI <dalton.smith@apiabroad.com>
2022-10-28 09:52:24 -0400

    add commit

--- resources/views/components/search.blade.php
+++ resources/views/components/search.blade.php
@@ -5,7 +5,7 @@
             <input type="hidden" name="category" value="{{ request('category') }}">
         @endif
         <input class="src-input"  name="search" type="text" placeholder="Search"  value = "{{ request('search') }}">
-        {{--                            <button class="src-btn" type="submit"><i class="ion-ios-search-strong"></i></button>--}
+        {{--                            <button class="src-btn" type="submit"><i class="ion-ios-search-strong"></i></button>--}}
     </form>
 
 
